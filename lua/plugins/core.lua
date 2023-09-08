a60424848bc44c33b3598bd2ce75fed7ad2ac2dd
bigzhu <vermiliondun@gmail.com>
2023-09-08 16:35:26 +0800

    my config

---
+++ lua/plugins/core.lua
@@ -0,0 +1,31 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "jakewvincent/mkdnflow.nvim",
+    rocks = "luautf8", -- Ensures optional luautf8 dependency is installed
+    config = function()
+      require("mkdnflow").setup({
+        links = {
+          style = "markdown",
+          name_is_source = true,
+          conceal = true,
+          context = 0,
+          transform_implicit = false,
+          transform_explicit = false,
+        },
+        mappings = {
+          MkdnFoldSection = { "n", "<leader>q" },
+        },
+      })
+    end,
+    lazy = false,
+  },
+}
