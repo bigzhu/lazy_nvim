13621fd130d85a9dd0547398cb2da104d512d484
bigzhu <vermiliondun@gmail.com>
2023-09-10 00:18:21 +0800

    删除不要的部分

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,6 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-package.path = package.path .. ";~/.luarocks/share/lua/5.1/magick/init.lua;"
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
