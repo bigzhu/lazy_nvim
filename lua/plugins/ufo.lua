086511dd991533005a8d997a3d22e1a85418ee42
bigzhu <vermiliondun@gmail.com>
2023-09-10 21:48:57 +0800

    避免自动折叠

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,19 @@
+return {
+  -- use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
+  {
+    "kevinhwang91/nvim-ufo",
+    lazy = false,
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    config = function()
+      require("ufo").setup({
+
+        provider_selector = function(bufnr, filetype, buftype)
+          vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+          return { "treesitter", "indent" }
+        end,
+      })
+    end,
+  },
+}
