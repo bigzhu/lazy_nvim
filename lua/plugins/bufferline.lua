896d9eee2d12d1674ad4b6cde778161fb0f6f1aa
bigzhu <vermiliondun@gmail.com>
2023-09-11 22:59:37 +0800

    Close all other visible buffers

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bc", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close all other visible buffers" },
+    },
+  },
+}
