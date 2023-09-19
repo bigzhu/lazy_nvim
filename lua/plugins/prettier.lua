f41b2095703de311ccfde799d4948a7c2f6286a8
bigzhu <vermiliondun@gmail.com>
2023-09-19 12:41:11 +0800

    vimwiki 不能用, 会覆盖 md 的文件类型

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,26 @@
+return {
+  -- {
+  --   "MunifTanjim/prettier.nvim",
+  --   lazy = false,
+  --   opts = function(_, opts)
+  --     local prettier = require("prettier")
+  --     prettier.setup({
+  --       bin = "prettierd", -- or `'prettierd'` (v0.23.3+)
+  --       filetypes = {
+  --         "css",
+  --         "graphql",
+  --         "html",
+  --         "javascript",
+  --         "javascriptreact",
+  --         "json",
+  --         "less",
+  --         "markdown",
+  --         "scss",
+  --         "typescript",
+  --         "typescriptreact",
+  --         "yaml",
+  --       },
+  --     })
+  --   end,
+  -- },
+}
