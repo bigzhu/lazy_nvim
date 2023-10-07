6952ae6d3c907b70b60223274aa23d085adf8ff9
bigzhu <vermiliondun@gmail.com>
2023-10-07 11:21:11 +0800

    暂时解决 tree-sitter-dart 的 bug

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 return {
-  -- "nvim-treesitter/nvim-treesitter",
-  -- -- branch = "master",
-  -- -- commit = "33eb472b459f1d2bf49e16154726743ab3ca1c6d",
-  --
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  commit = "f2778bd1a28b74adf5b1aa51aa57da85adfa3d16",
+
   -- config = function()
   --   local parser = require("nvim-treesitter.parsers").get_parser_configs()
   --   parser.dart = {
@@ -13,29 +13,32 @@ return {
   --     },
   --   }
   -- end,
-  -- opts = {
-  --   -- ensure_installed = "all",
-  --   ensure_installed = {
-  --     "dart",
-  --     "go",
-  --     "html",
-  --     "javascript",
-  --     "jsonc",
-  --     "lua",
-  --     "python",
-  --     "tsx",
-  --     "typescript",
-  --     "markdown",
-  --     "markdown_inline",
-  --     "regex",
-  --     "json",
-  --     "yaml",
-  --     "toml",
-  --     "bash",
-  --     "css",
-  --     "scss",
-  --     "vim",
-  --     "yaml",
-  --   },
-  -- },
+  opts = {
+    -- ensure_installed = "all",
+    ensure_installed = {
+      "dart",
+      "go",
+      "html",
+      "javascript",
+      "jsonc",
+      "lua",
+      "python",
+      "tsx",
+      "typescript",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "json",
+      "yaml",
+      "toml",
+      "bash",
+      "css",
+      "scss",
+      "vim",
+      "yaml",
+    },
+  },
+  "nvim-treesitter-textobjects",
+  branch = "master",
+  commit = "35a60f093fa15a303874975f963428a5cd24e4a0",
 }
