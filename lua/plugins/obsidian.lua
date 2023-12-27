f45d351bd00ab1c31bf67291532b14315d64324f
bigzhu <vermiliondun@gmail.com>
2023-12-27 13:26:08 +0800

    有 datetime frontmatter 时候不会再报错

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -38,7 +38,8 @@ return {
       local out = { id = note.id, tags = note.tags }
       -- `note.metadata` contains any manually added fields in the frontmatter.
       -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      -- if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      if note.metadata ~= nil then
         for k, v in pairs(note.metadata) do
           out[k] = v
         end
