8c0d8eff2f675be3de0df2bd887432543ba7471b
bigzhu <vermiliondun@gmail.com>
2024-01-19 11:24:29 +0800

    进一步解决 dart TS 的问题

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,45 +1,77 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  commit = "f2778bd1a28b74adf5b1aa51aa57da85adfa3d16",
-
-  -- config = function()
-  --   local parser = require("nvim-treesitter.parsers").get_parser_configs()
-  --   parser.dart = {
-  --     install_info = {
-  --       url = "https://github.com/UserNobody14/tree-sitter-dart",
-  --       files = { "src/parser.c", "src/scanner.c" },
-  --       revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4", -- The last commit before the snail speed
-  --     },
-  --   }
-  -- end,
   opts = {
-    -- ensure_installed = "all",
+    indent = {
+      enable = true,
+      disable = { "dart" },
+    },
     ensure_installed = {
-      "graphql",
       "dart",
-      "go",
+      "bash",
       "html",
       "javascript",
-      "jsonc",
+      "json",
       "lua",
-      "python",
-      "tsx",
-      "typescript",
       "markdown",
       "markdown_inline",
+      "python",
+      "query",
       "regex",
-      "json",
-      "yaml",
-      "toml",
-      "bash",
-      "css",
-      "scss",
+      "tsx",
+      "typescript",
       "vim",
       "yaml",
     },
   },
-  "nvim-treesitter-textobjects",
-  branch = "master",
-  commit = "35a60f093fa15a303874975f963428a5cd24e4a0",
 }
+
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--
+--   -- branch = "master",
+--   -- commit = "f2778bd1a28b74adf5b1aa51aa57da85adfa3d16",
+--
+--   -- config = function()
+--   --   local parser = require("nvim-treesitter.parsers").get_parser_configs()
+--   --   parser.dart = {
+--   --     install_info = {
+--   --       url = "https://github.com/UserNobody14/tree-sitter-dart",
+--   --       files = { "src/parser.c", "src/scanner.c" },
+--   --       revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4", -- The last commit before the snail speed
+--   --     },
+--   --   }
+--   -- end,
+--   opts = {
+--     indent = {
+--       enable = true,
+--       disable = { "dart" },
+--     },
+--     -- ensure_installed = "all",
+--     ensure_installed = {
+--       "graphql",
+--       "dart",
+--       "go",
+--       "html",
+--       "javascript",
+--       "jsonc",
+--       "lua",
+--       "python",
+--       "tsx",
+--       "typescript",
+--       "markdown",
+--       "markdown_inline",
+--       "regex",
+--       "json",
+--       "yaml",
+--       "toml",
+--       "bash",
+--       "css",
+--       "scss",
+--       "vim",
+--       "yaml",
+--     },
+--   },
+--   -- "nvim-treesitter-textobjects",
+--   -- branch = "master",
+--   -- commit = "35a60f093fa15a303874975f963428a5cd24e4a0",
+-- }
